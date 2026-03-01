0300504973dcd463f9d0d6dee4ca2383f62b070a
vaziety <79341513+koukyoukoku@users.noreply.github.com>
2026-03-01 07:11:14 +0700

    Delete a.js

--- a.js
+++
@@ -1,5 +0,0 @@
-import youtubesearchapi from "youtube-search-api";
-
-youtubesearchapi.GetListByKeyword("yume wo akiramenaide")
-    .then((result) => { console.log(result); })
-    .catch((error) => { console.error(error); });
